b20ec782711ad2715bb52e9c9f7a0a50927b8127
Cloud <smirnovceo@yandex.ru>
2021-10-12 15:18:29 +0500

    Fix bug with positional args and move far exeption catch
    Plus refactor naming for cleancode and fix typehints misstakes

--- links.py
+++ links.py
@@ -3,10 +3,6 @@ import requests
 from dotenv import load_dotenv
 
 
-BITLY_TOKEN = os.getenv('TOKEN')
-HEADERS = {'Authorization': f'Bearer {BITLY_TOKEN}'}
-
-
 def convert_from_http_url(url: str) -> str:
     if url.startswith('http'):
         return url.split('//')[1]
@@ -21,11 +17,12 @@ def convert_to_http_url(url: str) -> str:
         return url
 
 
-def is_bitlink(url: str):
+def is_bitlink(url: str, headers: dict) -> bool:
     url = convert_from_http_url(url)
     if url.startswith('bit.ly'):
         api_url = f'https://api-ssl.bitly.com/v4/bitlinks/{url}'
-        resp = requests.get(api_url, headers=HEADERS)
+        resp = requests.get(api_url, headers=headers)
+        resp.raise_for_status()
         if resp.ok:
             return True
         else:
@@ -33,24 +30,21 @@ def is_bitlink(url: str):
     return False
 
 
-def shorten_link(url: str) -> None:
-    uri = 'bitlinks'
-    api_url = f'https://api-ssl.bitly.com/v4/{uri}'
+def shorten_link(url: str, headers: dict) -> str:
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks'
     url = convert_to_http_url(url)
     payload = {
         'long_url': f'{url}'
     }
-    try:
-        resp = requests.post(api_url, headers=HEADERS, json=payload)
-        resp.raise_for_status()
-        response = dict(resp.json())
-        bitlink = response['link']
-        return bitlink
-    except requests.exceptions.HTTPError as ex:
-        print(ex, 'Неправильная ссылка, попробуйте еще раз...', sep='\n')
+
+    resp = requests.post(api_url, headers=headers, json=payload)
+    resp.raise_for_status()
+    response = dict(resp.json())
+    bitlink = response['link']
+    return bitlink
 
 
-def count_clicks(bitlink: str):
+def count_clicks(bitlink: str, headers: dict) -> int:
     bitlink = convert_from_http_url(bitlink)
     api_uri = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
     payload = {
@@ -59,27 +53,29 @@ def count_clicks(bitlink: str):
         'size': '',
         'unit_reference': '',
     }
-    try:
-        resp = requests.get(api_uri, headers=HEADERS, params=payload)
-        resp.raise_for_status()
-        response = dict(resp.json())
-        clicks = response['total_clicks']
-        return clicks
-    except requests.exceptions.HTTPError as ex:
-        print(ex, 'Неправильная ссылка, попробуйте еще раз...', sep='\n')
+
+    resp = requests.get(api_uri, headers=headers, params=payload)
+    resp.raise_for_status()
+    response = dict(resp.json())
+    clicks = response['total_clicks']
+    return clicks
 
 
 def main() -> None:
     load_dotenv()
+    TOKEN = os.getenv('BITLY_TOKEN')
+    HEADERS = {'Authorization': f'Bearer {TOKEN}'}
 
     url = input()
-    if is_bitlink(url):
-        result = count_clicks(url)
-    else:
-        result = shorten_link(url)
-    
-    if result:
-        print(result)
+    try:
+        if is_bitlink(url, headers=HEADERS):
+            result = count_clicks(url, headers=HEADERS)
+        else:
+            result = shorten_link(url, headers=HEADERS)
+        if result or result == 0:
+            print(result)
+    except requests.exceptions.HTTPError as ex:
+        print(ex, 'Неправильная ссылка, попробуйте еще раз...', sep='\n')
 
 
 if __name__ == '__main__':
