e3ee276cb58004e00ca7c1c42d07136ac07fdaab
Cloud <smirnovceo@yandex.ru>
2021-10-13 16:39:20 +0500

    Refactoring with naming for selfdoc

--- links.py
+++ links.py
@@ -50,8 +50,8 @@ def count_clicks(bitlink: str, headers: dict) -> int:
     resp = requests.get(api_uri, headers=headers, params=payload)
     resp.raise_for_status()
     response = dict(resp.json())
-    clicks = response['total_clicks']
-    return clicks
+    total_clicks = response['total_clicks']
+    return total_clicks
 
 
 def main() -> None:
@@ -62,10 +62,11 @@ def main() -> None:
     url = input()
     try:
         if is_bitlink(url, headers=headers):
-            result = count_clicks(url, headers=headers)
+            total_clicks = count_clicks(url, headers=headers)
+            print(total_clicks)
         else:
-            result = shorten_link(url, headers=headers)
-        print(result)
+            shorted_link = shorten_link(url, headers=headers)
+            print(shorted_link)
     except requests.exceptions.HTTPError as ex:
         print(ex, 'Неправильная ссылка, попробуйте еще раз...', sep='\n')
 
